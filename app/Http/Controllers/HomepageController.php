99e0aa069b1ab6eb527454431f2faf6801c6e992
Tim Harvey <timharveydev@protonmail.com>
2023-06-05 13:03:23 +0100

    Create HomepageController, route and page template

---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class HomepageController extends Controller
+{
+    /**
+     * Display the Homepage.
+     */
+    public function index(): Response
+    {
+        return Inertia::render('Homepage', [
+            'text' => 'Hello World!'
+        ]);
+    }
+}
---
+++ resources/js/Pages/Homepage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    text: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <h1>{{ text }}</h1>
+</template>
+
+<style scoped>
+
+</style>
