8741be1ac286ccd997250705da85ef46b4a5aecc
Tim Harvey <timharveydev@protonmail.com>
2023-06-15 08:59:38 +0100

    Build footer

---
+++ resources/js/Layouts/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="absolute bottom-0 left-0 w-full bg-gray-900 text-white text-center text-sm py-4 lg:py-8">
+        <span>&copy; {{ new Date().getFullYear()}} Liz Harvey Art. All Rights Reserved.</span>
+    </footer>
+</template>
---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Header from "@/Layouts/Header.vue";
+import Footer from "@/Layouts/Footer.vue";
+
+defineProps({
+    userIsLoggedIn: {
+        type: Boolean,
+        required: true,
+    }
+})
+</script>
+
+<template>
+    <Header :user-is-logged-in="userIsLoggedIn"/>
+    <slot />
+    <Footer />
+</template>
