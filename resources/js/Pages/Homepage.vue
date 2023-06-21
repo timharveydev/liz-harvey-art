4573e4e2ef5ac731caef0b0e3092a79ad9c9f897
Tim Harvey <timharveydev@protonmail.com>
2023-06-21 21:11:03 +0100

    Refactor homepage

--- resources/js/Pages/Homepage.vue
+++ resources/js/Pages/Homepage.vue
@@ -23,8 +23,12 @@ defineProps({
 
     <MainLayout :bg-image="bgImage" :bg-brightness="75">
         <div class="h-full flex flex-col justify-center items-center text-center">
-            <span v-if="byline" class="text-xl sm:text-2xl mb-2">{{ byline }}</span>
-            <h1 class="font-serif text-6xl sm:text-7xl lg:text-8xl mb-8">{{ pageHeading }}</h1>
+            <span v-if="byline" class="text-xl sm:text-2xl mb-2">
+                {{ byline }}
+            </span>
+            <h1 class="font-serif text-6xl sm:text-7xl lg:text-8xl mb-8">
+                {{ pageHeading }}
+            </h1>
         </div>
     </MainLayout>
 </template>
